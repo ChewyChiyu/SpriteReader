52a9e753cdf47c94af7a7faec259b2d347970b1c
BuildTools <unconfigured@null.spigotmc.org>
2017-05-18 21:28:54 -0700

    files

--- src/spriteReader/SpriteReaderUI.java
+++ src/spriteReader/SpriteReaderUI.java
@@ -5,6 +5,7 @@ import java.awt.CardLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.io.File;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
@@ -15,20 +16,37 @@ import javax.swing.JScrollPane;
 import javax.swing.JViewport;
 
 public class SpriteReaderUI{
-	private File[] listedFilesDesktop = new File(System.getProperty("user.home") + "/Desktop").listFiles();
-	private File[] listedFilesDownloads = new File(System.getProperty("user.home")+"/Downloads/").listFiles();
+	
 	private JFrame masterFrame = new JFrame("Sprite Reader");
 	private CardLayout cardLayout = new CardLayout();
 	private JPanel masterDisplayPane = new JPanel(new BorderLayout());
 	private JPanel masterScrollPane = new JPanel(cardLayout);
 	private JPanel masterChoosePane = new JPanel(new BorderLayout());
-	
+	ArrayList<File> listedFilesDesktop = new ArrayList<File>();
+	ArrayList<File> listedFilesDownloads = new ArrayList<File>();
 	
 	public static void main(String[] args){
 		new SpriteReaderUI();
 	}
 	SpriteReaderUI(){
+		processFiles();
 		panel();
+	}
+	void processFiles(){
+		File[] listedFilesDesktop = new File(System.getProperty("user.home")+"/Desktop/").listFiles();
+		File[] listedFilesDownloads = new File(System.getProperty("user.home")+"/Downloads/").listFiles();
+		
+		for(File f : listedFilesDesktop){
+			if(f.toString().substring(f.toString().length()-4).equalsIgnoreCase(".png")){
+				this.listedFilesDesktop.add(f);
+			}
+		}
+		for(File f : listedFilesDownloads){
+			if(f.toString().substring(f.toString().length()-4).equalsIgnoreCase(".png")){
+				this.listedFilesDownloads.add(f);
+			}
+		}
+	
 	}
 	void panel(){
 		setUpMasterChoosePane();
@@ -40,41 +58,7 @@ public class SpriteReaderUI{
 		masterFrame.setVisible(true);
 		masterFrame.setResizable(false);
 		masterFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
-		
-		
-		
-		
-		
-//		JPanel desktop = setUpDisplayChoosePanenew JPanel();
-//		JScrollPane sp = new JScrollPane(desktop,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-//		desktop.setLayout(new GridLayout(listedFiles.length, 1));
-//		for(File f : listedFiles){
-//			JButton b = new JButton(f.toString());
-//			b.addActionListener(e -> {
-//				if(f.toString().substring(f.toString().length()-4).equalsIgnoreCase(".png")){
-//					try{
-//					new SelectedImage(ImageIO.read(f));
-//					}catch(Exception a) { 
-//					}
-//				}else{
-//					JOptionPane.showMessageDcardLayoutialog(frame,
-//						    "Cannot open, only png",
-//						    "Inane error",
-//						    JOptionPane.ERROR_MESSAGE);
-//				}
-//			});
-//			desktop.add(b);
-//		}
-//		sp.getViewport ().setScrollMode (JViewport.BACKINGSTORE_SCROLL_MODE );
-//		sp.getViewport ().setScrollMode (JViewport.SIMPLE_SCROLL_MODE );
-//		frame.add(sp);setUpDisplayChoosePane
-//		frame.setPreferredSize(new Dimension(500,200));
-//		frame.pack();cardLayout
-//		frame.setVisible(true);
-//		frame.setResizable(false);
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+	
 	}
 	void setUpMasterChoosePane(){
 		JButton desktop = new JButton("DESKTOP");
@@ -97,24 +81,17 @@ public class SpriteReaderUI{
 		JPanel downloads = new JPanel();
 		
 
-		desktop.setLayout(new GridLayout(listedFilesDesktop.length, 1));
-		downloads.setLayout(new GridLayout(listedFilesDownloads.length, 1));
+		desktop.setLayout(new GridLayout(listedFilesDesktop.size(), 1));
+		downloads.setLayout(new GridLayout(listedFilesDownloads.size(), 1));
 
 		for(File f : listedFilesDesktop){
 			JButton b = new JButton(f.toString());
 			b.setPreferredSize(new Dimension(400,70));
 			b.addActionListener(e -> {
-				if(f.toString().substring(f.toString().length()-4).equalsIgnoreCase(".png")){
 					try{
 					new SelectedImage(ImageIO.read(f));
 					}catch(Exception a) { 
 					}
-				}else{
-					JOptionPane.showMessageDialog(masterFrame,
-						    "Cannot open, only png",
-						    "Inane error",
-						    JOptionPane.ERROR_MESSAGE);
-				}
 			});
 			desktop.add(b);
 		}
@@ -122,17 +99,10 @@ public class SpriteReaderUI{
 			JButton b = new JButton(f.toString());
 			b.setPreferredSize(new Dimension(400,70));
 			b.addActionListener(e -> {
-				if(f.toString().substring(f.toString().length()-4).equalsIgnoreCase(".png")){
 					try{
 					new SelectedImage(ImageIO.read(f));
 					}catch(Exception a) { 
 					}
-				}else{
-					JOptionPane.showMessageDialog(masterFrame,
-						    "Cannot open, only png",
-						    "Inane error",
-						    JOptionPane.ERROR_MESSAGE);
-				}
 			});
 			downloads.add(b);
 		}
